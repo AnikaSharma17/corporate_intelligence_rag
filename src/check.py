c7e274c01e89d603c270412b7d0c09c93a12ef7e
AnikaSharma17 <133599434+AnikaSharma17@users.noreply.github.com>
2025-07-19 22:10:05 +0530

    Update check.py

--- src/check.py
+++ src/check.py
@@ -1,15 +1,15 @@
 from neo4j import GraphDatabase
 
 URI = "neo4j://127.0.0.1:7687"
-AUTH = ("neo4j", "Anika2005")  # ← Only if this is your DB password
+AUTH = ("neo4j", "your_psw")
 
 try:
     driver = GraphDatabase.driver(URI, auth=AUTH)
     with driver.session() as session:
         result = session.run("RETURN 1")
-        print("✅ Connection successful:", result.single())
+        print("Connection successful:", result.single())
 except Exception as e:
-    print("❌ Connection failed:", e)
+    print("Connection failed:", e)
 
 import google.generativeai as genai
 
